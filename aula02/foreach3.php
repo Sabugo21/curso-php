90f8b2c27541cdd021065b81685feee6c22c5678
Bruno Rossi <brossif21@gmail.com>
2018-05-26 15:46:49 -0300

    doido

--- aula02/foreach3.php
+++ aula02/foreach3.php
@@ -1,18 +1,54 @@
-<?php
+<?php 
+
+// Array $pessoa  -> Nome, Idade e Email
+// Exibir na tela o conteúdo de cada informação seguindo o exemplo;
+// Meu nome é $$$
+// Tenho $$$ anos
+// Meu email é $$$
+
+$pessoa = [
+	'nome' => 'Lucas', 
+	'idade' => 24, 
+	'email' => 'lucas@lucas.com'
+	];
+
+echo ' Meu nome é ' . $pessoa['nome'] . '<br>';
+echo ' Tenho ' . $pessoa['idade'] . ' anos' . '<br>';
+echo ' Meu email é ' . $pessoa['email'] . '<br>';
+
+// Arrray PessoaS onde teremos 3 pessoa
+// cada pessoa possui Nome, Idade e Email
+// Exibir na tela o conteúdo de cada informação seguindo o exemplo;
+// Meu nome é $$$
+// Tenho $$$ anos
+// Meu email é $$$
 
 $pessoas = [
-0=>[
-'nome' =>'Bruno',
-'idade' => 36
-],
-
-1=>[
-'nome' => 'Jose',
-'idade' => 65
-],
-
-2=>[
-'nome'=> 'Maria',
-'idade'=> 22
-]
+	[
+		'nome' => 'Lucas Marques',
+		'idade' => 24,
+		'email'=> 'lucasmarques73@hotmail.com'
+	],
+	[
+		'nome' => 'João Silva',
+		'idade' => 32,
+		'email'=> 'j.silva@hotmail.com'
+	],
+	[
+		'nome' => 'Maria Lucia',
+		'idade' => 40,
+		'email'=> 'lucia.maria@hotmail.com'
+	]
 ];
+
+foreach ($pessoas as $pessoa) {
+echo '<hr>';	
+
+echo ' Meu nome é ' . $pessoa['nome'] . '<br>';
+
+echo ' Tenho ' . $pessoa['idade'] . ' anos' . '<br>';
+
+echo ' Meu email é ' . $pessoa['email'] . '<br>';
+
+echo '<hr>';
+}
