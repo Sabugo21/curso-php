20011cb0c06433e71fcc03bea1e8d489cb19d613
Sabugo21 <39443882+Sabugo21@users.noreply.github.com>
2018-06-11 09:55:58 -0300

    Add files via upload

---
+++ deletar.php
@@ -0,0 +1,13 @@
+<?php 
+require 'conexao.php';
+$id = 3;
+$query = "DELETE FROM usuarios WHERE id={$id}";
+echo '<pre>';
+echo $query;
+echo '<br>';
+$result = mysqli_query($con,$query);
+if($result){
+	echo 'Ok';
+} else {
+	echo 'Erro ao deletar';
+}
---
+++ inserir.php
@@ -0,0 +1,20 @@
+<?php 
+require 'conexao.php';
+/* 
+	nome
+	email
+	senha
+*/
+$nome = 'Paula Maria';
+$email = 'paula.maria@4linux.com.br';
+$senha = '123';
+$query = "INSERT INTO usuarios(nome,email,senha) VALUES ('{$nome}','{$email}', '{$senha}')";
+echo '<pre>';
+echo $query;
+echo '<br>';
+$result = mysqli_query($con,$query);
+if($result){
+	echo 'Ok';
+} else {
+	echo 'Erro ao inserir';
+}
